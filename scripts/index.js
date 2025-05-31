82b01d02fc88b9a668a7601a63a772d765f7bb39
Kenneth Hillsman <kenneth.hillsman@gmail.com>
2025-05-31 09:07:40 -0500

    Project 4 Stage 3 update

--- scripts/index.js
+++ scripts/index.js
@@ -30,12 +30,12 @@ const profileEditModal = document.querySelector("#profile-edit-modal");
 
 console.log(profileEditButton);
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal-opened");
 });
 
 const modalCloseButton = document.querySelector("#modal-close-button");
 
 console.log(modalCloseButton);
 modalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal-opened");
 });
