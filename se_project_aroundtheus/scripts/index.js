16bcc0bb9106b5bd09d70dbf179d4a06ac3a62a8
Kenneth Hillsman <kenneth.hillsman@gmail.com>
2026-07-02 16:16:25 -0500

    Sprint 4 Final Stage

---
+++ se_project_aroundtheus/scripts/index.js
@@ -0,0 +1,41 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+console.log(profileEditButton);
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal-opened");
+});
+
+const modalCloseButton = document.querySelector("#modal-close-button");
+
+console.log(modalCloseButton);
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal-opened");
+});
